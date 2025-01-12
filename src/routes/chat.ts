803ef7979bf83d278e7e253b631da3f71c703428
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-01-12 14:45:43 +0100

    Add chat routes

---
+++ src/routes/chat.ts
@@ -0,0 +1,24 @@
+import express, { Router } from "express";
+import { isAuthenticated } from "@/middlewares/authenticate";
+import * as chatController from "@/controllers/chat/Chat"
+const router: Router = express.Router();
+
+router.use(isAuthenticated);
+
+// Create private chat
+router.post("/private", chatController.createPrivateChat);
+
+// Send a message
+router.post("/:chatId/message", chatController.sendMessage);
+
+// Add a member to chat
+router.post("/:chatId/member", chatController.addMemberToChat);
+
+// Remove a member from chat
+router.delete("/:chatId/member", chatController.removeMemberFromChat);
+
+// Get chat details
+router.get("/:chatId", chatController.getChatDetails);
+
+
+export default router;
